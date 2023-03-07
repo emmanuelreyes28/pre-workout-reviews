2233fe0eb3625a7ffd68fc9d1d5005729f7047be
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-03-06 23:45:40 -0800

    Update login form

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,14 +6,35 @@ export default function login() {
       {/* <h1 className="text-2xl font-semibold text-gray-900">Login</h1> */}
       <form className="mt-12" action="" method="POST">
         <div className="relative">
+          <label for="email">Email Address</label>
           <input
             id="email"
             name="email"
             type="email"
-            className="peer h-10 w-full border-b-2 border-gray-300 
-            text-gray-900 placeholder-transparent focus:outline-none
-            focus:border-b-blue-900"
             placeholder="Email Address"
+            className="peer h-10 w-full border-gray-300 
+            text-gray-900 focus:outline-none
+            focus:border-b-blue-900"
+          />
+          <div className="mt-10 relative">
+            <label for="password">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              className="peer h-10 w-full border-gray-300 
+              text-gray-900 focus:outline-none focus:border-b-blue-900"
+              placeholder="Password"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Sign in"
+            className="mt-20 px-4 py-2 rounded bg-rose-500 
+            hover:bg-rose-400 text-white font-semibold 
+            text-center block w-full focus:outline-none focus:ring 
+            focus:ring-offset-2 focus:ring-rose-500 focus:ring-opacity-80 
+            cursor-pointer"
           />
         </div>
       </form>
