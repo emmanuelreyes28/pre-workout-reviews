a262efbf246a12562ffd042a85c598783f2aa919
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-03-28 23:18:39 -0700

    add styling to form component

--- components/Form.js
+++ components/Form.js
@@ -72,91 +72,148 @@ function Form({ formId, productForm }) {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Pre-Workout Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="productName"
-          value={product.productName}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="brand">Brand:</label>
-        <input
-          type="text"
-          id="brand"
-          name="brand"
-          value={product.brand}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="0">Benefit 1:</label>
-        <input
-          type="text"
-          id="0"
-          name="benefits"
-          value={product.benefits[0]}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="1">Benefit 2:</label>
-        <input
-          type="text"
-          id="1"
-          name="benefits"
-          value={product.benefits[1]}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="2">Benefit 3:</label>
-        <input
-          type="text"
-          id="2"
-          name="benefits"
-          value={product.benefits[2]}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="caffeine">Caffeine Per Serving:</label>
-        <input
-          type="number"
-          id="caffeine"
-          name="caffeinePerScoop"
-          value={product.caffeinePerScoop}
-          onChange={handleChange}
-          required
-        />
-        {/* change open label to question that determines boolean value */}
-        <label>Does pre-workout have an open label?</label>
-        <input
-          type="radio"
-          id="openLabel"
-          name="openLabel"
-          value={true}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="openLabel">Yes</label>
-        <input
-          type="radio"
-          id="proprietaryBlend"
-          name="openLabel"
-          value={false}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="proprietaryBlend">No</label>
-        <label htmlFor="image">Image URL:</label>
-        <input
-          type="text"
-          id="image"
-          name="image"
-          value={product.image}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit">Submit</button>
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+            Pre-Workout Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="productName"
+            value={product.productName}
+            onChange={handleChange}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="brand" className="block text-gray-700 font-bold mb-2">
+            Brand:
+          </label>
+          <input
+            type="text"
+            id="brand"
+            name="brand"
+            value={product.brand}
+            onChange={handleChange}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="0" className="block text-gray-700 font-bold mb-2">
+            Benefit 1:
+          </label>
+          <input
+            type="text"
+            id="0"
+            name="benefits"
+            value={product.benefits[0]}
+            onChange={handleChange}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="1" className="block text-gray-700 font-bold mb-2">
+            Benefit 2:
+          </label>
+          <input
+            type="text"
+            id="1"
+            name="benefits"
+            value={product.benefits[1]}
+            onChange={handleChange}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="2" className="block text-gray-700 font-bold mb-2">
+            Benefit 3:
+          </label>
+          <input
+            type="text"
+            id="2"
+            name="benefits"
+            value={product.benefits[2]}
+            onChange={handleChange}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="caffeine"
+            className="block text-gray-700 font-bold mb-2"
+          >
+            Caffeine Per Serving:
+          </label>
+          <input
+            type="number"
+            id="caffeine"
+            name="caffeinePerScoop"
+            value={product.caffeinePerScoop}
+            onChange={handleChange}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">
+            Does this pre-workout have an open label?
+          </label>
+          <div className="block">
+            <input
+              type="radio"
+              id="openLabel"
+              name="openLabel"
+              value={true}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="openLabel" className="text-gray-700 font-bold mb-2">
+              Yes
+            </label>
+          </div>
+          <div className="block">
+            <input
+              type="radio"
+              id="proprietaryBlend"
+              name="openLabel"
+              value={false}
+              onChange={handleChange}
+              required
+            />
+            <label
+              htmlFor="proprietaryBlend"
+              className="text-gray-700 font-bold mb-2"
+            >
+              No
+            </label>
+          </div>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="image" className="block text-gray-700 font-bold mb-2">
+            Image URL:
+          </label>
+          <input
+            type="text"
+            id="image"
+            name="image"
+            value={product.image}
+            onChange={handleChange}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <button
+          type="submit"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 hover:bg-gray-700 hover:text-white active:bg-gray-800"
+        >
+          Submit
+        </button>
       </form>
     </>
   );
