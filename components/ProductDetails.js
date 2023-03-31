3c7eafbcc3c1cc2751a98c21cc2fdac7ce05ff43
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-03-30 23:29:54 -0700

    create product details component

---
+++ components/ProductDetails.js
@@ -0,0 +1,10 @@
+export default function ProductDetails() {
+  return (
+    <>
+      <div>
+        <div>Image</div>
+        <div>Details/Info</div>
+      </div>
+    </>
+  );
+}
--- src/pages/pre-workouts/[id].jsx
+++ src/pages/pre-workouts/[id].jsx
@@ -1,7 +1,10 @@
+import ProductDetails from "components/ProductDetails";
+
 export default function PreWorkout() {
   return (
     <>
       <h1>Unique Pre-Workout Product</h1>
+      <ProductDetails />
     </>
   );
 }
