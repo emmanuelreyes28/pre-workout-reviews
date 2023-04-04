d86ac5b693df2e353f53f00dc926fffff1609034
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-04-03 23:40:06 -0700

    fetch product details given document id

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,8 +1,38 @@
-export default function ProductDetails() {
+import dbConnect from "lib/dbConnect";
+import Product from "models/Product";
+import { useEffect, useState } from "react";
+
+export default function ProductDetails({ productId }) {
+  const [product, setProduct] = useState(null);
+
+  //fetch product data from api
+  //run everytime productId prop change (dependency)
+  useEffect(() => {
+    async function fetchProduct() {
+      try {
+        const response = await fetch(`/api/product/${productId}`);
+        const { data } = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    // fetch product only if product id prop is received
+    if (productId) {
+      fetchProduct();
+    }
+  }, [productId]);
+
+  //show loading if product details have not yet been fetched
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <div>
-        <div>Image</div>
+        <div>{product.productName}</div>
         <div>Details/Info</div>
       </div>
     </>
--- src/pages/pre-workouts/[id].jsx
+++ src/pages/pre-workouts/[id].jsx
@@ -1,10 +1,14 @@
+import { useRouter } from "next/router";
 import ProductDetails from "components/ProductDetails";
 
 export default function PreWorkout() {
+  const router = useRouter();
+  const { id } = router.query; //grab the document id from url and pass it as prop
+  console.log(id);
   return (
     <>
       <h1>Unique Pre-Workout Product</h1>
-      <ProductDetails />
+      <ProductDetails productId={id} />
     </>
   );
 }
