01b0177354406670bf7b54c1e8b4534757f7b45c
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-04-05 23:27:43 -0700

    style product details

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -30,28 +30,39 @@ export default function ProductDetails({ productId }) {
 
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <Image
-              src={product.image}
-              alt="pre-workout"
-              width={400}
-              height={400}
-            />
-            <div>
-              <h1>{product.productName}</h1>
-              <p>{product.brand}</p>
-              <p>Rating: {product.overallRating}/5</p>
-              <div>
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex flex-col md:flex-row items-center gap-8">
+          <div className="w-full md:w-1/2">
+            <div className="w-full h-full flex justify-center items-center">
+              <Image
+                src={product.image}
+                alt="pre-workout"
+                width={400}
+                height={400}
+                className="object-contain"
+              />
+            </div>
+          </div>
+          <div className="w-full md:w-1/2">
+            <h1 className="text-3xl font-bold">{product.productName}</h1>
+            <p className="text-lg mb-2">{product.brand}</p>
+            <p>Rating: {product.overallRating}/5</p>
+            <div className="mb-4">
+              <p className="font-medium mb-1">Benefits:</p>
+              <div className="flex flex-wrap">
                 {product.benefits.map((benefit) => (
-                  <span key={benefit._id}>{benefit}&nbsp;</span>
+                  <span
+                    key={benefit._id}
+                    className="bg-gray-200 rounded-full py-1 px-2 text-sm font-medium mr-2 mb-2"
+                  >
+                    {benefit}&nbsp;
+                  </span>
                 ))}
               </div>
-              <div>
-                <p>Description:</p>
-                <p>{product.description}</p>
-              </div>
+            </div>
+            <div>
+              <p>Description:</p>
+              <p>{product.description}</p>
             </div>
           </div>
         </div>
