c6001861cfb06f100488653eb767e4c49a8180b7
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-04-26 22:50:51 -0700

    edit review form

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function ReviewForm({ productId }) {
   const [product, setProduct] = useState(null);
@@ -32,7 +33,43 @@ export default function ReviewForm({ productId }) {
   // after submit clicked take user back to that specific product page
   return (
     <>
-      <h1>{product.productName}</h1>
+      <div className="container mx-auto px-4 py-8">
+        <div className="border-b-2 border-stone-200">
+          <h2 className="text-2xl font-semibold">Create Review</h2>
+          <div className="mt-4 flex items-center pb-4">
+            <div className="shrink-0 mr-4">
+              <Image
+                src={product.image}
+                alt="pre-workout"
+                width={100}
+                height={100}
+                className="object-contain"
+              />
+            </div>
+            <div>
+              <span className="text-xl">{product.productName}</span>
+              <span className="font-sm text-zinc-500"> By {product.brand}</span>
+            </div>
+          </div>
+        </div>
+        <div className="border-b-2 border-stone-200 mt-4 pb-4">
+          <div className="text-xl pb-3">
+            <h3>Overall Rating</h3>
+          </div>
+          <div>
+            <input
+              type="number"
+              id="rating"
+              name="rating"
+              required
+              className="w-10"
+              min={1}
+              max={5}
+            />
+            <span> Out of 5</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
