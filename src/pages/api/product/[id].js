ed9866196b5d30e33d48a1eaa0b37936a5351d4c
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-04-29 00:28:23 -0700

    update unique product post request

--- src/pages/api/product/[id].js
+++ src/pages/api/product/[id].js
@@ -23,12 +23,44 @@ export default async function handler(req, res) {
       break;
     case "POST":
       try {
-        console.log(req.body);
-        const product = await Product.findById(id);
+        const { id } = req.query;
+        const product = await Product.findById(id).exec();
+        //console.log(req.body);
+        //const product = await Product.findById(id);
         if (!product) {
           // if the product is not found, return a 404 response
           return res.status(404).json({ success: false });
         }
+
+        // create new review object
+        const newReview = {
+          user,
+          content,
+          rating,
+          helpfulCount: 0,
+        };
+
+        // add the new review to the product's reviews array
+        product.reviews.push(newReview);
+
+        // calculate the overall and taste rating based on the new review
+        const numReviews = product.reviews.length;
+        const overallRating =
+          product.reviews.reduce((sum, review) => sum + review.rating, 0) /
+          numReviews;
+        const tasteRating =
+          product.reviews.reduct(
+            (sum, review) => sum + review.rating * 0.7,
+            0
+          ) / numReviews;
+
+        // update the product's overall and taste rating
+        product.overallRating = overallRating;
+        product.tasteRating = tasteRating;
+
+        // save the updated product to the database
+        await product.save();
+
         res.status(201).json({ success: true, data: product });
       } catch (error) {
         console.error(error);
