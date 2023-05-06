539b4bdf194d27c780a6ad3be6f3291286caa1c0
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-05-05 23:21:59 -0700

    install react-icons and update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <>
-      <h1>This is the footer</h1>
+      <footer className="bg-red-200 py-12">
+        <div className="flex justify-center text-center">
+          <p className="text-base font-medium">Emmanuel Reyes &copy; {year}</p>
+        </div>
+        <div className="flex justify-center text-center py-2">
+          <Link href={"https://github.com/emmanuelreyes28/pre-workout-reviews"}>
+            <FaGithub className="text-black hover:text-white" size={24} />
+          </Link>
+        </div>
+      </footer>
     </>
   );
 }
